4a0816636d877b2eb09da21874279f13e651fa1c
dungvunguyen2810 <62170664+dungvunguyen2810@users.noreply.github.com>
2020-06-24 07:56:29 +0700

    1

---
+++ user.js
@@ -0,0 +1,27 @@
+const express = require('express');
+var router = express.Router();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb+srv://asm:12345@cluster0-9snej.mongodb.net/test';
+
+router.get("/", async (req, res) => {
+    res.render("login");
+});
+
+router.post("/", async (req, res) => {
+    let un = req.body.un;
+    let pass = req.body.pass;
+    if (un == "admin" && pass == "admin") {
+        let client = await MongoClient.connect(url);
+        let dbo = client.db("asm");
+
+        let result = await dbo.collection("products").find({}).toArray();
+        //res.render("allProduct", { product: result });
+        var fullUrl = req.protocol + "://" + req.get("host") + "/product";
+        res.redirect(fullUrl);
+    } else {
+        res.end("Login failed");
+    }
+});
+
+module.exports = router;
